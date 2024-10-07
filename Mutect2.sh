8b3e230591625654952dad68b89b030d8810ad73
Ricardo Gore <rjhgore@gmail.com>
2024-10-06 22:02:39 -0700

    Update Mutect2.sh

--- Mutect2.sh
+++ Mutect2.sh
@@ -36,10 +36,131 @@ if [ ! -f "CHKPNT_DIR/${bs}_Mutect2.chkpt" ]; then
 
 else
     echo "file already exists. Omitting Mutect2"
-fi 
+fi
 
-echo "Mutect2 was done for $bs group"
+if [ ! -f "S4_GATK_Mutect2_DIR/${bs}.f.vcf.gz" ]; then
+
+    call="gatk FilterMutectCalls -R $reference -V S4_GATK_Mutect2_DIR/${bs}.vcf.gz -O S4_GATK_Mutect2_DIR/${bs}.f.vcf.gz"
+
+    echo $call
+    eval $call
+
+else
+    echo "Filtered file already exists. Omitting FilterMutectCalls"
+fi
+
+echo "Mutect2 and FilterMutectCalls was done for $bs group"
 
 done
 
+# exit
+
+# extract SNPs & INDELS
+
+# SNPs
+
+mkdir -p S5_GATK_SelectVariants_DIR
+
+if [ ! -f "S5_GATK_SelectVariants_DIR/${bs}.f.snp.vcf.gz" ]; then
+
+    call="gatk SelectVariants -R $reference -V S4_GATK_Mutect2_DIR/${bs}.f.vcf.gz --select-type SNP -O S5_GATK_SelectVariants_DIR/${bs}.f.snp.vcf.gz"
+
+    echo $call
+    eval $call
+
+else
+    echo "Filtered file already exists. Omitting SelectVariants (SNPs) and Funcotator"
+fi
+
+# INDELS
+
+if [ ! -f "S5_GATK_SelectVariants_DIR/${bs}.f.indels.vcf.gz" ]; then
+
+    call="gatk SelectVariants -R $reference -V S4_GATK_Mutect2_DIR/${bs}.f.vcf.gz --select-type INDEL -O S5_GATK_SelectVariants_DIR/${bs}.f.indels.vcf.gz"
+
+    echo $call
+    eval $call
+
+else
+    echo "Filtered file already exists. Omitting SelectVariants (Indels) and Funcotator"
+fi
+
+# Annotation
+
+dataSourcesFolder=/LUSTRE/bioinformatica_data/genomica_funcional/rgomez/Human/hg38/funcotator/funcotator_dataSources.v1.8.hg38.20230908s
+
+if [ ! -f "$dataSourcesFolder/funcotator_dataSources.v1.8.hg38.20230908s.tar.gz" ]; then
+
+    call="gatk FuncotatorDataSourceDownloader --somatic --validate-integrity --extract-after-download --hg38"
+    eval $call
+else
+    echo "FuncotatorDataSourceDownloader already was ran"
+fi
+
+# Choose Variant type
+variant_type=indels
+
+if [ ! -f "S5_GATK_SelectVariants_DIR/${bs}.f.${variant_type}.functotated.vcf.gz" ]; then
+
+    call="gatk Funcotator \
+	--variant S5_GATK_SelectVariants_DIR/${bs}.f.${variant_type}.vcf.gz \
+	--reference $reference \
+	--ref-version hg38 \
+	--data-sources-path $dataSourcesFolder \
+	--output S5_GATK_SelectVariants_DIR/${bs}.f.${variant_type}.functotated.vcf.gz \
+	--output-file-format VCF"
+    
+    eval $call
+
+    call="gatk VariantsToTable -V S5_GATK_SelectVariants_DIR/${bs}.f.${variant_type}.functotated.vcf.gz -F AC -F AN -F DP -F AF -F FUNCOTATION -O S5_GATK_SelectVariants_DIR/${bs}.f.${variant_type}.functotated.table"
+
+    eval $call
+else
+    echo "Funcotator already was run for ${bs}.f.${variant_type}.functotated.vcf.gz"
+fi
+
+
+variant_type=snp
+
+if [ ! -f "S5_GATK_SelectVariants_DIR/${bs}.f.${variant_type}.functotated.vcf.gz" ]; then
+
+    call="gatk Funcotator \
+	--variant S5_GATK_SelectVariants_DIR/${bs}.f.${variant_type}.vcf.gz \
+	--reference $reference \
+	--ref-version hg38 \
+	--data-sources-path $dataSourcesFolder \
+	--output S5_GATK_SelectVariants_DIR/${bs}.f.${variant_type}.functotated.vcf.gz \
+	--output-file-format VCF"
+    
+    eval $call
+
+    call="gatk VariantsToTable -V S5_GATK_SelectVariants_DIR/${bs}.f.${variant_type}.functotated.vcf.gz -F AC -F AN -F DP -F AF -F FUNCOTATION -O S5_GATK_SelectVariants_DIR/${bs}.f.${variant_type}.functotated.table"
+
+    eval $call
+
+
+    
+else
+    echo "Funcotator already was run for ${bs}.f.${variant_type}.functotated.vcf.gz"
+fi
+
 exit
+
+#gatk SelectVariants -R ${ref} -V ${results}/raw_variants.vcf --select-type SNP -O ${results}/raw_snps.vcf
+#gatk SelectVariants -R ${ref} -V ${results}/raw_variants.vcf --select-type INDEL -O ${results}/raw_indels.vcf
+
+# annotate
+# https://github.com/kpatel427/YouTubeTutorials/blob/ac98e41ef30446b8afe40af247315d81389bfeaa/variant_filtering_annotation.sh#L39
+gatk Funcotator \
+	--variant ${results}/analysis-ready-snps-filteredGT.vcf \
+	--reference ${ref} \
+	--ref-version hg38 \
+	--data-sources-path /Users/kr/Desktop/demo/tools/functotator_prepackaged_sources/funcotator/hg38/funcotator_dataSources.v1.7.20200521g \
+	--output ${results}/analysis-ready-snps-filteredGT-functotated.vcf \
+	--output-file-format VCF
+
+    # Extract fields from a VCF file to a tab-delimited table
+
+gatk VariantsToTable \
+	-V ${results}/analysis-ready-snps-filteredGT-functotated.vcf -F AC -F AN -F DP -F AF -F FUNCOTATION \
+	-O ${results}/output_snps.table
